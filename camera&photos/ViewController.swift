cdac2ea2e22e0f5bfd481862c075049cd6dd84ab
Erin Martin <ecm8807@gmail.com>
2021-06-29 13:21:40 -0400

    Iteration 1 commit

--- camera&photos/ViewController.swift
+++ camera&photos/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    class viewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+        var imagePicker = UIImagePickerController()
+        
+        override func viewDidLoad(){
+            super.viewDidLoad()
+            imagePicker.delegate = self
+        }
+    
+    
+    @IBAction func takeSelfieTapped(_ sender: Any) {
+    imagePicker.sourceType = .camera
+    present(imagePicker, animated: true, completion: nil)
+}
+    @IBAction func photoLibraryTapped(_ sender: Any) {
+    imagePicker.sourceType = .photoLibrary
+    present(imagePicker, animated: true, completion: nil)
     }
 
+    @IBOutlet var displayImage: UIImageView!
 
+private func imagePickerController( picker:UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]){
+    if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+        displayImage.image = selectedImage
+    }
+    imagePicker.dismiss(animated: true, completion: nil)
+    }
+
+    }
 }
 
