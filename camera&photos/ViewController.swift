3a9aab0cd1d7d6c4fdd492cad86260d07ba789f2
Erin Martin <ecm8807@gmail.com>
2021-06-29 11:40:59 -0400

    Initial Commit

---
+++ camera&photos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  camera&photos
+//
+//  Created by Erin Martin on 6/29/21.
+//  Copyright © 2021 Erin Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
